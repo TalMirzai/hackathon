ea448f1f714ce45514a04df73bfc203d9725a665
TalMirzai <133582945+TalMirzai@users.noreply.github.com>
2025-02-17 21:48:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,8 @@ if page == "Welcome":
 
     # Add an image above the button (Replace with your GitHub image URL)
     image_url = "https://raw.githubusercontent.com/TalMirzai/hackathon/refs/heads/main/AssWholeAI-Team.png"
+    st.image(image_url, width=200)  
+
     st.image(image_url, use_container_width=True)
 
     # Add spacing before the button
